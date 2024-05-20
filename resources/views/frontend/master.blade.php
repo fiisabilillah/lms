3f4bdb62c9d65b804d5d05cd0b67bc11b1a7827b
fiisabilillah <officialteam548@gmail.com>
2024-05-21 00:16:38 +0700

    bagian 20

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -15,6 +15,8 @@
     <!-- Favicon --> 
     <link rel="icon" sizes="16x16" href="{{ asset('frontend/images/favicon.png') }}">
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- inject:css -->
     <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.css') }}">
@@ -117,7 +119,9 @@
  @endif 
 </script>
  
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
+@include('frontend.body.script')
 
 </body>
 </html>
