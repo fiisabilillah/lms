545cc0b550d1712c6d426f90a61164528ad5e630
fiisabilillah <officialteam548@gmail.com>
2024-05-20 23:49:14 +0700

    bagian 19

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\SubCategory;
 use App\Models\Course;
+use App\Models\User;
 use App\Models\Course_goal;
 use App\Models\CourseSection;
 use App\Models\CourseLecture;
@@ -52,5 +53,14 @@ class IndexController extends Controller
         $categories = Category::latest()->get();
         return view('frontend.category.subcategory_all', compact('courses', 'subcategory', 'categories'));
     } // End Method 
+
+
+     public function InstructorDetails($id){
+
+        $instructor = User::find($id);
+        $courses = Course::where('instructor_id',$id)->get();
+        return view('frontend.instructor.instructor_details',compact('instructor','courses'));
+
+    }// End Method 
 
 }
