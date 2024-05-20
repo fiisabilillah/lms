4e00cd057df5da1f095ccb8aa823bc3b4377e6ea
fiisabilillah <officialteam548@gmail.com>
2024-05-20 22:11:16 +0700

    bagian 16

---
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\Course;
+use App\Models\Course_goal;
+use App\Models\CourseSection;
+use App\Models\CourseLecture;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Illuminate\Support\Facades\Auth; 
+use Carbon\Carbon;
+
+class IndexController extends Controller
+{
+    public function CourseDetails($id,$slug){
+
+        $course = Course::find($id);
+        $goals = Course_goal::where('course_id',$id)->orderBy('id','DESC')->get();
+        
+        $ins_id = $course->instructor_id; 
+        $instructorCourses = Course::where('instructor_id',$ins_id)->orderBy('id','DESC')->get();
+
+        $categories = Category::latest()->get();
+
+        $cat_id = $course->category_id; 
+        $relatedCourses = Course::where('category_id',$cat_id)->where('id','!=',$id)->orderBy('id','DESC')->limit(3)->get();
+
+        return view('frontend.course.course_details',compact('course','goals','instructorCourses','categories','relatedCourses'));
+
+    } // End Method 
+}
