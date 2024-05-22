f379250a2aa8ccba06b193ed99bfe781aa816df6
fiisabilillah <officialteam548@gmail.com>
2024-05-23 02:16:55 +0700

    bagian 28

--- routes/web.php
+++ routes/web.php
@@ -199,6 +199,8 @@ Route::post('/coupon-apply', [CartController::class, 'CouponApply']);
 Route::get('/coupon-calculation', [CartController::class, 'CouponCalculation']);
 Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
 
+/// Checkout Page Route 
+Route::get('/checkout', [CartController::class, 'CheckoutCreate'])->name('checkout');
 
 
 
