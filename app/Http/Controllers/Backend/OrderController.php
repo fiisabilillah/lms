361e709824d17bce226bf2fab74842f6fd4ee6c9
fiisabilillah <officialteam548@gmail.com>
2024-05-23 16:46:08 +0700

    bagian 35

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -19,6 +19,10 @@ use App\Models\Coupon;
 use Illuminate\Support\Facades\Session;
 use App\Models\Payment;
 use App\Models\Order;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\DB;
+
+
 
 class OrderController extends Controller
 {
@@ -63,8 +67,11 @@ class OrderController extends Controller
     public function InstructorAllOrder(){
 
         $id = Auth::user()->id;
-        $orderItem = Order::where('instructor_id',$id)->orderBy('id','desc')->get();
-        return view('instructor.orders.all_orders',compact('orderItem'));
+        $latestOrderItem = Order::where('instructor_id',$id)->select('payment_id', DB::raw('MAX(id) as max_id'))->groupBy('payment_id');
+
+        $orderItem = Order::joinSub($latestOrderItem, 'latest_order', function($join) {
+            $join->on('orders.id', '=', 'latest_order.max_id');
+        })->orderBy('latest_order.max_id','DESC')->get();        return view('instructor.orders.all_orders',compact('orderItem'));
 
     }// End Method 
 
@@ -75,6 +82,21 @@ class OrderController extends Controller
         $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
 
         return view('instructor.orders.instructor_order_details',compact('payment','orderItem'));
+
+    }// End Method 
+
+
+    
+    public function InstructorOrderInvoice($payment_id){
+
+        $payment = Payment::where('id',$payment_id)->first();
+        $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
+
+        $pdf = Pdf::loadView('instructor.orders.order_pdf',compact('payment','orderItem'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
 
     }// End Method 
 
