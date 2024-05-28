45258ec9018b1bee641a9e6a95d64a4b1fdce834
fiisabilillah <officialteam548@gmail.com>
2024-05-29 03:40:56 +0700

    bagian 37

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::middleware('auth')->group(function () {
        // User My Course All Route 
        Route::controller(OrderController::class)->group(function(){
         Route::get('/my/course','MyCourse')->name('my.course'); 
+        Route::get('/course/view/{course_id}','CourseView')->name('course.view'); 
 
 
     });
